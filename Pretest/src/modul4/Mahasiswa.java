349ba0ad7af2872286c935afb828b56c315021cc
Samuelreinaldo12 <samuelreinaldo30@gmail.com>
2026-03-31 14:55:37 +0700

    aldo

---
+++ Pretest/src/modul4/Mahasiswa.java
@@ -0,0 +1,14 @@
+class Mahasiswa {
+    String nama;
+    int umur;
+    String jurusanStudi;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Mahasiswa mhsBaru = new Mahasiswa();
+        mhsBaru.nama = "Andi Susanto";
+        mhsBaru.umur = 20;
+        mhsBaru.jurusanStudi = "Teknik Informatika";
+    }
+}
